a896b75b7e4e06372278a2541e1948a6601bbd96
Omoju <omojumiller@gmail.com>
2016-08-23 16:07:07 -0700

    docs: update

--- tools.py
+++ tools.py
@@ -4,6 +4,12 @@ from sklearn import cross_validation
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
 
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import pca
+
 
 def shuffle_split_data(X, y, num_train):
     """ Shuffles and splits data into 75% training and 25% testing subsets,
@@ -135,3 +141,39 @@ def makeConfusionMatrix(clf, X_test, y_test):
     print "{:15}{:10}{:10}{:10}".format('Predicted', '', 'Female', 'Male')
     print "{:15}{:10}{:<10.3f}{:<10.3f}".format('', 'Female', cM[0][0], cM[0][1])
     print "{:15}{:10}{:<10.3f}{:<10.3f}".format('', 'Male', cM[1][0], cM[1][1])
+
+    
+def pca_results(good_data, pca):
+	'''
+	Create a DataFrame of the PCA results
+	Includes dimension feature weights and explained variance
+	Visualizes the PCA results
+	'''
+
+	# Dimension indexing
+	dimensions = dimensions = ['Dimension {}'.format(i) for i in range(1,len(pca.components_)+1)]
+
+	# PCA components
+	components = pd.DataFrame(np.round(pca.components_, 4), columns = good_data.keys())
+	components.index = dimensions
+
+	# PCA explained variance
+	ratios = pca.explained_variance_ratio_.reshape(len(pca.components_), 1)
+	variance_ratios = pd.DataFrame(np.round(ratios, 4), columns = ['Explained Variance'])
+	variance_ratios.index = dimensions
+
+	# Create a bar plot visualization
+	fig, ax = plt.subplots(figsize = (14,8))
+
+	# Plot the feature weights as a function of the components
+	components.plot(ax = ax, kind = 'bar');
+	ax.set_ylabel("Feature Weights")
+	ax.set_xticklabels(dimensions, rotation=0)
+
+
+	# Display the explained variance ratios
+	for i, ev in enumerate(pca.explained_variance_ratio_):
+		ax.text(i-0.40, ax.get_ylim()[1] + 0.05, "Explained Variance\n          %.4f"%(ev))
+
+	# Return a concatenated DataFrame
+	return pd.concat([variance_ratios, components], axis = 1)
