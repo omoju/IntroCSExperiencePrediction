df9b1002dbbff679352ff4e50aa24fae168d2388
Omoju <omojumiller@gmail.com>
2016-09-02 14:03:12 -0500

    docs: update

--- tools/tools.py
+++ tools/tools.py
@@ -52,71 +52,7 @@ def train_predict(clf, X_train, y_train, X_test, y_test):
     return train_time, F1_score_train, F1_score_test, prediction_time_test
 
 
-def print_Output_Table(clf, train_time, prediction_time, F1_score_train, F1_score_test, prntLatex):
-    """Print output table for classifier scores.
 
-    Keyword arguments:
-    clf -- classifier (scikit learn classifier model)
-    train_time -- array (float)
-    prediction_time -- array (float)
-    F1_score_train -- array (float)
-    F1_score_test -- array (float)
-    prntLatex -- 1 for print optimized for latex, 0 for regular printing
-    
-    """
-    
-    print "{:>53}".format(clf.__class__.__name__)
-    print "{:>53}".format('Training set size')
-    print "{:>30}{:>d}\t{:>d}\t{:>d}".format('',200,400,600)
-    print "-"*55
-
-    if prntLatex:
-        for i in range(1):
-            print "{:30}{:3}{:.3f}{:3}{:.3f}{:3}{:.3f}{:3}".format('Training time (secs)','&',
-                                           train_time[i], '&',train_time[i+1], '&',train_time[i+2], '\\\\')
-            print "{:30}{:3}{:.3f}{:3}{:.3f}{:3}{:.3f}{:3}".format('Prediction time (secs)','&',
-                                        prediction_time[i], '&',prediction_time[i+1], '&',prediction_time[i+2], '\\\\')
-            print "{:30}{:3}{:.3f}{:3}{:.3f}{:3}{:.3f}{:3}".format('F1 score for training set','&',
-                                          F1_score_train[i], '&',F1_score_train[i+1],'&',F1_score_train[i+2], '\\\\')
-            print "{:30}{:3}{:.3f}{:3}{:.3f}{:3}{:.3f}{:3}".format('F1 score for test set','&',
-                                          F1_score_test[i],'&',F1_score_test[i+1],'&',F1_score_test[i+2], '\\\\')
-    else:
-        for i in range(1):
-            print "{:30}{:.3f}\t{:.3f}\t{:.3f}".format('Training time (secs)',
-                                           train_time[i], train_time[i+1], train_time[i+2])
-            print "{:30}{:.3f}\t{:.3f}\t{:.3f}".format('Prediction time (secs)',
-                                           prediction_time[i], prediction_time[i+1], prediction_time[i+2])
-            print "{:30}{:.3f}\t{:.3f}\t{:.3f}".format('F1 score for training set',
-                                          F1_score_train[i], F1_score_train[i+1],F1_score_train[i+2])
-            print "{:30}{:.3f}\t{:.3f}\t{:.3f}".format('F1 score for test set',
-                                          F1_score_test[i],F1_score_test[i+1],F1_score_test[i+2])
-            
-
-def makeTable(clf, X_train, y_train, X_test, y_test, prntLatex):
-    """Print output table for classifier scores by creating scores based on incrementally growing the feature space.
-
-    Keyword arguments:
-    clf -- classifier (scikit learn classifier model)
-    X_test -- array of features (float)
-    y_test -- array of labels (float)
-    prntLatex -- 1 for print optimized for latex, 0 for regular printing
-    
-    """
-    train_set_size = [0] * 3
-    train_time = [0] * 3
-    prediction_time = [0] * 3
-    F1_score_train = [0] * 3
-    F1_score_test = [0] * 3
-
-    for i in xrange(100, 600, 200):
-        (train_time[(i//200)-1], F1_score_train[(i//200)-1], 
-         F1_score_test[(i/200)-1], prediction_time[(i//200)-1]) = train_predict(clf, 
-                                                              X_train[:i], y_train[:i], X_test, y_test)
-        train_set_size[(i//200)-1] = i
-
-
-    print_Output_Table(clf, train_time, prediction_time, F1_score_train, F1_score_test, prntLatex)
-    
 ## Confusion Matrix
 ## Predicted Class by Actual Class
 
